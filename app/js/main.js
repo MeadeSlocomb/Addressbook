0363dec184c3d32459e7bfc0f99a37651e644e65
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-14 17:44:51 -0400

    Delete function added, but introduced bug where icon menu only works on every other contact entry.

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,75 @@
-var allEntries = new EntryCollection();
+;(function (){
 
-allEntries.fetch().done(function(){
+  'use strict';
 
-  allEntries.each(function(model) {
-    addEntryToView(model.attributes);
+
+
+// Needed Variables
+
+var url = 'http://tiy-515.herokuapp.com/collections/meade-addressbook/';
+
+
+
+// Defines HTML layout of entries appended to page
+
+var addEntryToView = function(entry){
+
+  var entryHTML = "<li class='entry' id='" + entry._id + "'>";
+      entryHTML +=  "<div class='entryHeader'>";
+      entryHTML +=    "<span class='name'>" + entry.lastName + ", " + entry.firstName + "</span>";
+      entryHTML +=  "</div>";
+      entryHTML +=  "<ul class='infoList'>";
+      entryHTML +=    "<li class='email'>" + entry.email + "</li>";
+      entryHTML +=    "<li class='phone'>" + entry.phoneNumber + "</li>";
+      entryHTML +=    "<li class='twitter'>" + entry.twitter + "</li>";
+      entryHTML +=    "<li class='linkedIn'>" + entry.linkedIn + "</li>";
+      entryHTML +=  "</ul>";
+      entryHTML +=  "<div class='menu'>";
+      entryHTML +=    "<i class='fa fa-cog fa-lg'></i>";
+      entryHTML +=    "<i class='options fa fa-pencil'></i>";
+      entryHTML +=    "<i class='options fa fa-trash-o'></i>";
+      entryHTML +=  "</div>";
+      entryHTML += "</li>";
+
+  $('#addressBook').append(entryHTML);
+
+
+
+// Hover Styling on Properties Menu
+
+  $('.menu').on('mouseenter', function(e){
+    $(this).find('.fa-cog').toggleClass('fa-spin');
+    $(this).toggleClass('menuhover');
   });
 
-  // Hover Styling on Properties Menu
-
-$('.menu').on('mouseenter', function(){
-  $(this).find('.fa-cog').toggleClass('fa-spin');
-  $(this).toggleClass('menuhover');
-});
-
-$('.menu').on('mouseleave', function(){
-  $(this).find('.fa-cog').toggleClass('fa-spin');
-  $(this).toggleClass('menuhover');
-});
+  $('.menu').on('mouseleave', function(e){
+    $(this).find('.fa-cog').toggleClass('fa-spin');
+    $(this).toggleClass('menuhover');
+  });
 
 
-});
+
+// Delete Button
+
+  $('.entry').on('click', '.fa-trash-o', function(e){
+
+    e.preventDefault();
+
+    var idToDelete = $(this).parent().parent().attr('id');
+    var contactToDelete = $(this).parent().parent();
+
+    $.ajax({
+      url: url + idToDelete,
+      type: 'DELETE'
+    }).done( function () {
+    contactToDelete.fadeOut();
+    });
+  });
+};
+
+
+
+//Pulls values from HTML input and defines new instance using Backbone model
 
 var addEntry = function(e){
 
@@ -42,34 +92,13 @@ var addEntry = function(e){
   });
 
   allEntries.add(entry).save().success(function(data){
-    addEntrytoView(data);
+    addEntryToView(data);
   });
 
   this.reset();
 
 };
 
-var addEntryToView = function(entry){
-
-  var entryHTML = "<li class='entry' id='" + entry._id + "'>";
-      entryHTML +=  "<div class='entryHeader'>";
-      entryHTML +=    "<span class='name'>" + entry.lastName + ", " + entry.firstName + "</span>";
-      entryHTML +=  "</div>";
-      entryHTML +=  "<ul class='infoList'>";
-      entryHTML +=    "<li class='email'>" + entry.email + "</span>";
-      entryHTML +=    "<li class='phone'>" + entry.phoneNumber + "</span>";
-      entryHTML +=    "<li class='twitter'>" + entry.twitter + "</span>";
-      entryHTML +=    "<li class='linkedIn'>" + entry.linkedIn + "</span>";
-      entryHTML +=  "</ul>";
-      entryHTML +=  "<div class='menu'>";
-      entryHTML +=    "<i class='fa fa-cog fa-lg'></i>";
-      entryHTML +=    "<i class='options fa fa-pencil'></i>";
-      entryHTML +=    "<i class='options fa fa-trash-o'></i>";
-      entryHTML +=  "</div>";
-      entryHTML += "</li>";
-
-  $('#addressBook').prepend(entryHTML);
-};
 
 
 // Submit New Address Listener
@@ -78,3 +107,24 @@ $('#addEntry').on('submit', addEntry);
 
 
 
+// Displays saved entries on page
+
+var allEntries = new EntryCollection();
+
+allEntries.fetch().done(function(){
+
+  allEntries.each(function(model) {
+    addEntryToView(model.attributes);
+  });
+
+
+});
+
+
+
+
+
+
+
+
+}());
